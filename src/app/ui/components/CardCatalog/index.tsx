d6b7d68c2893f22de8cd15653580b5fd4b9a8567
Daniel <danielramire1996@gmail.com>
2024-09-27 12:05:05 -0500

    change root relative

--- src/app/ui/components/CardCatalog/index.tsx
+++ src/app/ui/components/CardCatalog/index.tsx
@@ -14,14 +14,14 @@ const trips: Trip[] = [
     description:
       "Disfruta de un breve viaje a la órbita lunar con vistas impresionantes.",
     price: "30,000,000 COP",
-    imageUrl: "src/assets/images/bronce.webp",
+    imageUrl: "../src/assets/images/bronce.webp",
   },
   {
     title: "Vuelta a la Luna",
     description:
       "Explora la cara oculta de la Luna y contempla sus misteriosos paisajes.",
     price: "45,000,000 COP",
-    imageUrl: "src/assets/images/plata.webp",
+    imageUrl: "../../../src/assets/images/plata.webp",
   },
   {
     title: "Super Espacial",
