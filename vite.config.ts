b5148109aa4ef83e948c277357ead68d658c262f
Daniel <danielramire1996@gmail.com>
2024-09-27 11:45:32 -0500

    change url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://sherna0303.github.io/web-space-travel-frontend/'
+  base: 'https://leinadanul.github.io/-moon_travel/'
 })
