446c0070a2daacb012eeb761121f6336036756d1
Daniel <danielramire1996@gmail.com>
2024-09-27 11:52:46 -0500

    Fix url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://leinadanul.github.io/-moon_travel/'
+  base: 'https://leinadanul.github.io/moon_travel/'
 })
